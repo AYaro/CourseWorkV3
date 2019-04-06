2e74353cafd66cdaa79bd33216540f1b121e2dc7
alexyaro <0370384@mail.ru>
2019-04-06 20:18:10 +0300

    :(

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav">|
+      <router-link to="/user">User</router-link>
     </div>
-    <router-view />
+    <router-view :hellomsg="msg"></router-view>
   </div>
 </template>
 
