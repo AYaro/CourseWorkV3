88f9472766ae72104092ac4ff14aab33f846adff
dunaevai135 <dunaevai135@gmail.com>
2019-04-09 17:24:29 +0300

    Merge

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -13,9 +13,31 @@ export default new Router({
       component: Display
     },
     {
-      path: '/user',
-      name: 'User',
-      component: User
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },{
+      path: "/contacts",
+      name: "contacts",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+          import(/* webpackChunkName: "about" */ "./views/Contacts.vue")
     }
   ]
 })
